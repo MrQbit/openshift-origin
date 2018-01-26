4b745e02aaf8e3071c6b029680610068ffcd4c00
MrQbit <ausiliom@gmail.com>
2018-01-26 18:21:07 +0100

    delete existing folder
    deleting existing folder

--- scripts/deployOpenShift.sh
+++ scripts/deployOpenShift.sh
@@ -515,6 +515,8 @@ EOF
 
 echo $(date) " - Cloning openshift-ansible repo for use in installation"
 
+runuser -l $SUDOUSER -c "rm -rf /home/$SUDOUSER/openshift-ansible"
+
 runuser -l $SUDOUSER -c "git clone -b release-3.7 https://github.com/openshift/openshift-ansible /home/$SUDOUSER/openshift-ansible"
 
 echo $(date) " - Running network_manager.yml playbook"
